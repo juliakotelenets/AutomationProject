fccbce95a02ec0585e98f6d7686b31c13965e01c
juliakotelenets <55530813+juliakotelenets@users.noreply.github.com>
2022-01-24 12:19:56 +0200

    Update SearchResultsPage.java

--- bbc/src/main/java/BBC1pages/SearchResultsPage.java
+++ bbc/src/main/java/BBC1pages/SearchResultsPage.java
@@ -11,9 +11,9 @@ public class SearchResultsPage extends BasePage {
     public SearchResultsPage(WebDriver driver) { super(driver); }
 
     @FindBy(xpath = "//a[contains(@class,'PromoLink')]")
-    private List<WebElement> listOfHeadline;
+    private List<WebElement> listOfHeadlines;
 
-    public WebElement getHeadlineByIndex(int index){return listOfHeadline.get(index);}
+    public WebElement getHeadlineByIndex(int index){return listOfHeadlines.get(index);}
 
     public String getTextOfHeadlineByIndex(int index){return getHeadlineByIndex(index).getText();}
 }
